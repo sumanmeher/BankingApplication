eeca9c766bddc4f083d7b4f834dfde41c2d87a84
Suman.Meher <Suman.Meher@LDIGITBGL-L0218.godigit.corp>
2023-08-03 12:24:12 +0530

    added new codes

--- src/main/java/com/java/digitTraing/TransactionHistory.java
+++
@@ -1,9 +0,0 @@
-package com.java.digitTraing;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet("/TransactionHistory")
-public class TransactionHistory extends HttpServlet {
-
-}
